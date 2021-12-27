f264130144b2d02533df90ef6aa981d8640b78d4
nabin bhattrai <bhattarainabin64@gmail.com>
2021-12-27 11:37:04 +0545

    deleted app formmethod

--- formmethod/post/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class Regis(forms.Form):
-    name=forms.CharField()
-    email=forms.EmailField()
-    password= forms.CharField()
--- validateform/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class StuReg(forms.Form):
-    name = forms.CharField()
-    email = forms.EmailField()
