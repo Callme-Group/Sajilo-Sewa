3dd20ca6fd30d7612b5df7296b06ba5f2b8a5028
nabin bhattrai <bhattarainabin64@gmail.com>
2022-01-03 19:49:08 +0545

    successfully added captcha verify

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,5 @@
+import urllib
+
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import RegisterUserFrom
@@ -5,8 +7,10 @@ from django.contrib import messages
 from .models import Profile
 from django.contrib.auth.models import User, auth
 import requests
+import json
 from django.conf import settings
-
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode
 
 # Create your views here.
 def dashboard(request):
@@ -36,25 +40,43 @@ def login(request):
     if request.method == 'POST':
         uname = request.POST['username']
         passwd = request.POST['password']
-
         user = auth.authenticate(username=uname, password=passwd)
-
-
         if user is not None:
             if not user.is_staff:
 
-
                 auth.login(request, user)
+                recaptcha_response = request.POST.get('g-recaptcha-response')
+                url = 'https://www.google.com/recaptcha/api/siteverify'
+                values = {
+                    'secret': settings.RECAPTCHA_PRIVATE_KEY,
+                    'response': recaptcha_response
+                }
+                data = urllib.parse.urlencode(values).encode()
+                req = urllib.request.Request(url,data=data)
+                response = urllib.request.urlopen(req)
+                result = json.loads(response.read().decode())
+                print(result)
+
+                ''' End reCAPTCHA validation '''
+                if result['success']:
+                    print('pass')
+                    messages.success(request, "Welcome ...")
+                    return redirect("/signup")
+
+                else:
+                    print('fail')
+                    messages.error(request, 'Invalid reCAPTCHA. Please try again.')
+                    return redirect('/login')
+
                 # return redirect("/signup")
-                messages.success(request, "Welcome ...")
-                return redirect("/signup")
-                #return HttpResponse("Success")
+
+                # return HttpResponse("Success")
 
             elif user.is_staff:
                 auth.login(request, user)
                 messages.success(request, "Welcome to ----.")
                 return redirect('/admin')
-                #return HttpResponse("Success")
+                # return HttpResponse("Success")
         else:
             messages.add_message(request, messages.ERROR, "Invalid Username and Password!")
             # return render(request, 'dashboard/login.html')
@@ -62,5 +84,3 @@ def login(request):
 
     else:
         return render(request, 'dashboard/login.html')
-
-
