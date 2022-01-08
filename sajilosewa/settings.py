2c8e69486877146ffc663d7ad48a71ffc0d47b08
nabin bhattrai <bhattarainabin64@gmail.com>
2022-01-08 21:56:28 +0545

    modified serviices

--- sajilosewa/settings.py
+++ sajilosewa/settings.py
@@ -83,10 +83,23 @@ WSGI_APPLICATION = 'sajilosewa.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Sajilo_Sewa',
+        'USER': 'root',
+        'PASSWORD': 'Nabin1412@',
+        'PORT': 3306,
+        'HOST': '127.0.0.1'
+
     }
 }
 
