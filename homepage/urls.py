4cf7affc8c12319769b2689b136447f7a461d312
200125_AdarshaKhadka_STW121COM <khadka03adrsh@gmail.com>
2022-01-22 16:53:31 +0545

    changepassword added in profile

--- homepage/urls.py
+++ homepage/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('profile/', views.profile, name='Profile')
+    path('profile/', views.profile, name='Profile'),
+    path('changepass/',views.changepass,name='changepass'),
     ]
 
--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,HttpResponseRedirect
 from .forms import ProfileForm
 from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.forms import PasswordResetForm
+
+from django.contrib.auth.forms import PasswordChangeForm
+
+
 from dashboard.auth import user_only
 from .models import Profile
 
@@ -20,3 +25,15 @@ def profile(request):
         'form': ProfileForm(instance=profile)
     }
     return render(request, 'homepage/profile.html', context)
+
+
+def changepass(request):
+    if request.method == "POST":
+        fm = PasswordChangeForm(user=request.user,data=request.POST)
+        if fm.is_valid():
+            fm.save()
+            return render(request,'homepage/profile.html')
+
+    else:
+        fm = PasswordChangeForm(user=request.user)
+    return render(request,'homepage/changepass.html',{'form':fm})
